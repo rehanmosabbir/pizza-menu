83d226b22426d2937b641a1c78d95d21edd54dc9
Md. Mosabbir Hossain <rehanmosabbir@gmail.com>
2023-09-05 09:54:08 +0600

    add Pizza component to index.js

--- src/index.js
+++ src/index.js
@@ -47,7 +47,24 @@ const pizzaData = [
 ];
 
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="pizza spinaci" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  );
 }
 
 // React 18
