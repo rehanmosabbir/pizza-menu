9ed724929a1af7e19542dc3f6a5bcd565bf60d68
Md. Mosabbir Hossain <rehanmosabbir@gmail.com>
2023-09-05 11:14:56 +0600

    add Header, Menu and Footer component to index.js

--- src/index.js
+++ src/index.js
@@ -49,12 +49,28 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
     </div>
   );
+}
+function Footer() {
+  return <h5>{new Date().toLocaleTimeString()}. We're currently open.</h5>;
 }
 
 function Pizza() {
